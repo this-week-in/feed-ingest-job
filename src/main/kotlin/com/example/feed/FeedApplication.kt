e4b5344fbd3be266eac415cb4d20254b12a75d58
Josh Long <starbuxman@gmail.com>
2018-01-03 02:12:04 -0800

    feed

--- src/main/kotlin/com/example/feed/FeedApplication.kt
+++ src/main/kotlin/com/example/feed/FeedApplication.kt
@@ -1,13 +1,20 @@
 package com.example.feed
 
 import com.rometools.rome.feed.synd.SyndEntry
+import org.apache.commons.logging.LogFactory
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.support.GenericApplicationContext
 import org.springframework.context.support.beans
 import org.springframework.core.io.UrlResource
+import org.springframework.core.task.TaskExecutor
 import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.integration.context.IntegrationContextUtils
 import org.springframework.integration.dsl.IntegrationFlows
 import org.springframework.integration.dsl.context.IntegrationFlowContext
 import org.springframework.integration.feed.dsl.Feed
@@ -16,13 +23,18 @@ import org.springframework.integration.metadata.MetadataStore
 import org.springframework.stereotype.Component
 import org.springframework.util.ReflectionUtils
 import pinboard.PinboardClient
+import java.time.Duration
+import java.time.Instant
+import java.time.ZoneId
 import java.util.*
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicLong
 
 /**
  * @author <a href="mailto:josh@joshlong.com">Josh Long</a>
  */
 @SpringBootApplication
+@EnableConfigurationProperties(IngestProperties::class)
 class FeedApplication
 
 class RedisMetadataStore(val stringRedisTemplate: StringRedisTemplate) : MetadataStore {
@@ -44,9 +56,50 @@ class RedisMetadataStore(val stringRedisTemplate: StringRedisTemplate) : Metadat
 			else null
 }
 
+@ConfigurationProperties("ingest")
+class IngestProperties(val inactivityThresholdInSeconds: Long = 60)
+
+@Component
+class InactivityMonitor(
+		ingestProperties: IngestProperties,
+		private val executor: TaskExecutor,
+		private val applicationContext: GenericApplicationContext) : InitializingBean {
+
+	private val window = Duration.ofSeconds(
+			ingestProperties.inactivityThresholdInSeconds).toMillis()
+
+	private val log = LogFactory.getLog(javaClass)
+
+	private val lastTick = AtomicLong(System.currentTimeMillis())
+
+	fun tick() {
+		this.lastTick.set(System.currentTimeMillis())
+	}
+
+	override fun afterPropertiesSet() {
+		this.executor.execute({
+			this.log.info("About to begin ${javaClass.name} thread.")
+			while (true) {
+				Thread.sleep(1000 * 1)
+				val now = System.currentTimeMillis()
+				val then = this.lastTick.get()
+				val diff = now - then
+				if (diff > window) {
+					this.log.info("There has been ${window}ms of inactivity. " +
+							"Calling ${applicationContext.javaClass.name}#close()")
+					this.applicationContext.close()
+				}
+			}
+		})
+	}
+}
+
 @Component
 class SimpleRunner(val ifc: IntegrationFlowContext,
-                   val pc: PinboardClient) : ApplicationRunner {
+                   val pc: PinboardClient,
+                   val inactivityMonitor: InactivityMonitor) : ApplicationRunner {
+
+	private val log = LogFactory.getLog(javaClass)
 
 	override fun run(args: ApplicationArguments) {
 
@@ -86,22 +139,22 @@ class SimpleRunner(val ifc: IntegrationFlowContext,
 		val title = syndEntry.title
 		val date = Date((syndEntry.updatedDate ?: syndEntry.publishedDate ?: Date()).time)
 		try {
-
-			println("------------------------------------------------")
-			println("processing $link")
+			log.info("Processing $link")
 			if (pc.getPosts(url = link).posts.isEmpty()) {
 				val post = pc.addPost(url = link, description = title,
-						tags = tags.toTypedArray(), dt = date, extended = "by ${authors.joinToString(" and ")}", shared = false, toread = false, replace = false)
+						tags = tags.toTypedArray(), dt = date, extended = "by ${authors.joinToString(" and ")}",
+						shared = false, toread = false, replace = false)
 				if (post) {
-					println("added $link ('$title') to Pinboard")
+					log.info("added $link ('$title') to Pinboard @ ${Instant.now().atZone(ZoneId.systemDefault())}")
 				}
 			}
+			this.inactivityMonitor.tick()
 		} catch (ex: Exception) {
+			log.error("couldn't process $link.", ex)
 			ReflectionUtils.rethrowException(ex)
 		}
 	}
 }
-
 
 fun main(args: Array<String>) {
 	SpringApplicationBuilder()
@@ -111,15 +164,11 @@ fun main(args: Array<String>) {
 							Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors())
 						}
 						bean {
-							SimpleRunner(ref(), ref())
+							SimpleRunner(ref(), ref(), ref())
 						}
-						/*	bean(IntegrationContextUtils.METADATA_STORE_BEAN_NAME) {
-								RedisMetadataStore(ref())
-							}
-							bean {
-								FeedFlowRegistrationRunner(ref(), ref(), ref())
-							}
-						 */
+						bean(IntegrationContextUtils.METADATA_STORE_BEAN_NAME) {
+							RedisMetadataStore(ref())
+						}
 					}
 			)
 			.sources(FeedApplication::class.java)
