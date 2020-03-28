edaeb79e27bcfaa8c207bd126c5f1a5577279f92
Josh Long <josh@joshlong.com>
2020-03-28 06:54:47 -0700

    deploy.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,13 +5,6 @@ JOB_NAME=${APP_NAME}
 SCHEDULER_SERVICE_NAME=scheduler-joshlong
 REDIS_NAME=redis-cache
 
-#https://docs.cloudfoundry.org/devguide/deploy-apps/healthchecks.html#setting_health_checks
-
-
-## DONT DELETE THIS
-cf push -b java_buildpack -u none --no-route --no-start -p target/${APP_NAME}.jar ${APP_NAME}
-## DONT DELETE THIS
-
 cf d -f ${APP_NAME}
 
 ## scheduler
@@ -22,20 +15,15 @@ cf s | grep ${REDIS_NAME} || cf cs rediscloud 100mb ${REDIS_NAME}
 cf bs ${APP_NAME} ${REDIS_NAME}
 
 cf push -b java_buildpack -u none --no-route --no-start -p target/${APP_NAME}.jar ${APP_NAME}
-#cf push --no-start  -p target/${APP_NAME}.jar ${APP_NAME}
+
 cf set-env ${APP_NAME} PINBOARD_TOKEN "${PINBOARD_TOKEN}"
 cf set-env ${APP_NAME} SPRING_PROFILES_ACTIVE cloud
 cf set-env ${APP_NAME} JBP_CONFIG_OPEN_JDK_JRE '{ jre: { version: 11.+}}'
 
 cf restart ${APP_NAME}
 
-#cf set-health-check $APP_NAME none
-#
-#cf restage ${APP_NAME}
-#
-## delete the job IF it already exists
-#cf jobs  | grep $JOB_NAME && cf delete-job -f ${JOB_NAME}
-#cf create-job ${APP_NAME} ${JOB_NAME} ".java-buildpack/open_jdk_jre/bin/java org.springframework.boot.loader.JarLauncher"
-#cf schedule-job ${JOB_NAME} "1 * ? * *"
-#
-#cf run-job ${JOB_NAME}
+cf jobs  | grep $JOB_NAME && cf delete-job -f ${JOB_NAME}
+cf create-job ${APP_NAME} ${JOB_NAME} ".java-buildpack/open_jdk_jre/bin/java org.springframework.boot.loader.JarLauncher"
+cf schedule-job ${JOB_NAME} "*/15 * ? * *"
+
+cf run-job ${JOB_NAME}
